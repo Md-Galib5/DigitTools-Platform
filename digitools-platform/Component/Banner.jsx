b2b39af135b095283bd00ed5096ec761951f6ea7
Md-Galib5 <mdgalibcp05@gmail.com>
2026-04-01 23:39:06 +0600

    fixing bugs

--- digitools-platform/Component/Banner.jsx
+++ digitools-platform/Component/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
         {/* Left Content */}
         <div className="space-y-4">
           <div className="inline-flex items-center gap-2 bg-blue-500/10 border border-blue-500/30 text-[#4F39F6] text-sm font-medium px-5 py-2 rounded-full">
-            ✨ New: AI-Powered Tools Available
+            <img src="../assets/Group 5.png"></img> New: AI-Powered Tools Available
           </div>
 
           <h1 className="text-5xl lg:text-5xl font-bold leading-tight tracking-tighter">
